8186c535b934c8458125646dce8320f1d7278c89
Beril (Beth) <90333992+bberildundar@users.noreply.github.com>
2023-06-29 16:45:19 +0200

    tests for TransactionModel and TransactionService

---
+++ src/test/java/nl/inholland/gnasBank/models/TransactionTest.java
@@ -0,0 +1,36 @@
+package nl.inholland.gnasBank.models;
+
+import io.cucumber.java.Before;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class TransactionTest {
+    private Transaction transaction;
+
+    @BeforeEach
+    void init(){
+        transaction = new Transaction();
+    }
+
+    @Test
+    void newTransactionShouldNotBeNull(){
+        Assertions.assertNotNull(transaction);
+    }
+
+    @Test
+    void transactionDateShouldBeNow(){
+        LocalDateTime now = LocalDateTime.now();
+        transaction.setTimeStamp(now);
+
+        LocalDate transactionDate = transaction.getTimeStamp().toLocalDate();
+        LocalDate currentDate = LocalDate.now();
+
+        Assertions.assertEquals(currentDate, transactionDate);
+    }
+
+}
